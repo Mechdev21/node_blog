b708956be33bea89234b1366b195fd1e07bf66d5
Orji Favour <favour@titanname.com>
2024-07-03 10:32:50 +0100

    blog

--- auth.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { registerUser } = require('.//controller/auth.controll');
-
-router.post('/register', registerUser);
-
-module.exports = router;
--- index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const authRoute = require('./auth');
-
-router.use('/api/auth', authRoute);
-
-
-module.exports = router;
